ccc7df9491e767dacbbe430c676b3525d53e8110
rafajpr <39386290+rafajpr@users.noreply.github.com>
2020-11-11 02:48:51 -0300

    AAAAAA
    AAAAAAAAAAAAA

--- ZodiacoWS/src/java/dao/UsuarioDAO.java
+++ ZodiacoWS/src/java/dao/UsuarioDAO.java
@@ -1,71 +1,28 @@
 package dao;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import db.Database;
 import java.util.ArrayList;
-import java.util.List;
 import model.Usuario;
-import java.sql.*;
 
 public class UsuarioDAO {   
-    String db;   
     
     public UsuarioDAO(){
-        Path path = Paths.get("UsuarioDAO.java");
-        path = path.toAbsolutePath();
-        path = path.getParent();
-        path = path.getParent();
-        this.db = "jdbc:sqlite:" + path.toString() + "/Horoscope.db";
+        
     }
     
     public boolean adicionarUsuario(Usuario a){
-        String SQL = "INSERT INTO User(name, email, signo, plano) VALUES (?,?,?,?)";
-        Connection conn = null;
-        try{
-            conn = DriverManager.getConnection(db);
-            PreparedStatement pstmt = conn.prepareStatement(SQL);
-            pstmt.setString(1,a.getNickname());
-            pstmt.setString(2,a.getEmail());
-            pstmt.setString(3,a.getSigno());
-            pstmt.setString(4,a.getPlano());
-            pstmt.executeUpdate();
-            
-        }catch(SQLException e){
-            System.out.println(e.getMessage());
-            return false;
-        }finally{
-            try{
-                if(conn != null){
-                    conn.close();
-                }
-            }catch(SQLException ex){
-                System.out.println(ex.getMessage());
-            }
-        }
-        return true;
+        Database.getUsuarios().add(a);
+            return true;
     }
     
     public Usuario findUsuario(String email){
-        Usuario usuario = null;
-        Connection conn = null;
-        String SQL = "SELECT * FROM User WHERE User.email = ?";
-        try{
-            conn = DriverManager.getConnection(db);
-            PreparedStatement pstm = conn.prepareStatement(SQL);
-            pstm.setString(1, email);
-            ResultSet rs = pstm.executeQuery();
-            usuario = new Usuario(rs.getString("name"), rs.getString("plano"), rs.getString("email"), rs.getString("signo"));
-        }catch(SQLException e){
-            System.out.println(e.getMessage());
-        }finally{
-          try{
-                if(conn != null){
-                    conn.close();
-                }
-            }catch(SQLException ex){
-                System.out.println(ex.getMessage());
-            }  
+        ArrayList<Usuario> usuarios;
+        usuarios = Database.getUsuarios();
+        for(int i = 0; i < usuarios.size(); i++){
+            if(usuarios.get(i).getEmail().equals(email)){
+                return usuarios.get(i);
+            }
         }
-        return usuario;
+        return null;
     }
 }
--- ZodiacoWS/src/java/ws/CavaleiroBronzeWS.java
+++ ZodiacoWS/src/java/ws/CavaleiroBronzeWS.java
@@ -3,14 +3,16 @@ package ws;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
-import dao.UsuarioDAO;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import model.Usuario;
+import org.json.simple.parser.ParseException;
 
 @WebService(serviceName = "CavaleiroBronzeWS")
 public class CavaleiroBronzeWS {
     
     @WebMethod(operationName = "msgDia")
-    public String msgDia(@WebParam(name = "user") Usuario user) {
+    public String msgDia(@WebParam(name = "user") Usuario user) throws IOException, MalformedURLException, ParseException {
         String msg = user.getDailyHoroscope();
         return msg;
     }
